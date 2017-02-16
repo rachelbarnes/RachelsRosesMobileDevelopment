f55714c72536f4a1ecd5b4f002a5d1cd05efc750
rachelbarnes <rachelbarnesjn116@yahoo.com>
2017-02-16 17:43:44 -0500

    First commit

---
+++ RachelBarnesMobileDevelopmentTests/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RachelBarnesMobileDevelopmentTests.Startup))]
+namespace RachelBarnesMobileDevelopmentTests
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
