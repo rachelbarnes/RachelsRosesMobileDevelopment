dedee2fef678adc1824648f6194d74b2cb37fe05
rachelbarnes <rachelbarnesjn116@yahoo.com>
2017-03-02 18:06:54 -0500

    Working on setting up the entity framework

--- RachelBarnesMobileDevelopment/Controllers/HomeController.cs
+++ RachelBarnesMobileDevelopment/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 
 namespace RachelBarnesMobileDevelopment.Controllers {
-    public class AccountCOntroller : Controller {
+    public class HomeController : Controller {
         public ActionResult Index() {
             return View();
         }
@@ -21,5 +21,27 @@ namespace RachelBarnesMobileDevelopment.Controllers {
 
             return View();
         }
+        public ActionResult IngredientDensityTest() {
+            var context = new RachelsRosesMobileDevelopmentEntities(); 
+            var IngDen = new IngredientDensities();
+            IngDen.ingredient_type = "all purpose flour";
+            IngDen.density = 5m;
+            context.IngredientDensities.Add(IngDen);
+            context.SaveChanges();
+            return View();       
+        }
     }
 }
+//abstractvar reader = new ReadIngredientDensityFile();
+//var myIngredientDensityFile = reader.splitColonDelimitedFileInsertIntoDictionary(ReadIngredientDensityFile.filename);
+//   using (var context = new MyRecipesAndIngredientsDatabaseContext()) {
+//foreach (KeyValuePair<string, decimal> pair in myIngredientDensityFile) {
+//   IngredientDensity IngDen = new IngredientDensity {
+//       ingredient_type = pair.Key,
+//       density = pair.Value
+//   };
+//context.IngredientDensities.Add(IngDen);
+//}
+
+//context.SaveChanges();
+//   }
---
+++ RachelBarnesMobileDevelopment/Reader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO; 
+namespace RachelBarnesMobileDevelopment {
+    public class Reader {
+        public string filename = @"C:\Users\Rachel\Documents\Visual Studio 2015\Projects\RachelsRosesMobileDevelopment\RachelBarnesMobileDevelopment\IngredientDensitiesTxtFIle"; 
+        //this is currently a hardcoded file
+
+                //public Dictionary<string, decimal> PopulateIngredientDensityDictionary() {
+        //    string finalString;
+        //    using (var reader = new StreamReader(filename)) {
+        //        StreamReader currentLine;
+        //        while (reader.ReadLine() != null) {
+
+        //        }
+        //    }
+    }
+}
