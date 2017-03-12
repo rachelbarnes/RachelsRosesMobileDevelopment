28ce95ddfcf2028555a0282c77eb6f1b62164725
rachelbarnes <rachelbarnesjn116@yahoo.com>
2017-03-12 14:17:16 -0400

    Adding some more integrated tests and adding another few stories

--- IntegrationTests/BasicDBRoundTripTest.cs
+++ IntegrationTests/BasicDBRoundTripTest.cs
@@ -16,7 +16,7 @@ namespace IntegrationTests {
             using (var context = new RachelsRosesMobileDevelopmentEntities()) {
                 db.RecreateDatabase(context);
                 var IngDen = new IngredientDensities();
-                IngDen.Name= "all purpose flour";
+                IngDen.Name = "all purpose flour";
                 IngDen.Density = 5m;
                 context.IngredientDensities.Add(IngDen);
                 context.SaveChanges();
@@ -39,16 +39,60 @@ namespace IntegrationTests {
             using (var context = new RachelsRosesMobileDevelopmentEntities()) {
                 db.RecreateDatabase(context, "IngredientDensities", IngredientDensityColumns);
                 var IngDen = new IngredientDensities();
-                IngDen.Name= "bread flour";
+                IngDen.Name = "bread flour";
                 IngDen.Density = 5.4m;
                 context.IngredientDensities.Add(IngDen);
                 context.SaveChanges();
-
                 var densitiesCount =
                         (from d in context.IngredientDensities
                          select d).Count();
+                Assert.AreEqual(1, densitiesCount);
+            }
+        }
+        [TestMethod]
+        public void CreateAndInsertIntoIngredientsTable() {
+            var db = new DatabaseStuff();
+            var IngredientsColumns = new List<string>() {
+                "Id int not null identity(1,1) primary key",
+                "Name nvarchar(70)",
+                "Recipe_Id int",
+                "Measurement nvarchar(70)",
+                "Type nvarchar(50)",
+                "Classification nvarchar(50)",
+                "Selling_Weight nvarchar(50)"
+            };
+            using (var context = new RachelsRosesMobileDevelopmentEntities()) {
+                db.RecreateDatabase(context, "Ingredients", IngredientsColumns);
+                var Ing = new Ingredients();
+                Ing.Id = 1;
+                Ing.Name = "Bread Flour";
+                Ing.Measurement = "6 cups"; 
+                Ing.Selling_Weight = "5 lb";
+                Ing.Recipe_Id = 1;
+                Ing.Type = "Bread Flour";
+                context.Ingredients.Add(Ing);
+                context.SaveChanges();
 
-                Assert.AreEqual(1, densitiesCount); 
+                var ingredientsCount =
+                        (from i in context.Ingredients
+                         select i).Count();
+                Assert.AreEqual(1, ingredientsCount);
+            }
+            [TestMethod]
+            public void CreateAndInsertIntoPantryTable() {
+            var db = new DatabaseStuff();
+            var PantryColumns = new List<string>() {
+                "Pantry_Id int not null identity(1,1) primary key",
+                "Name nvarchar(60) not null",
+                "Ounces_Consumed decimal(6,2) not null",
+                "Ounces_Remaining decimal(6,2) not null",
+                "Expired int not null",
+                "Expiring_Soon int not null"
+            };using (var context = new RachelsRosesMobileDevelopmentEntities()) {
+                db.RecreateDatabase(context, "Pantry", PantryColumns);
+                var pan = new Pantry();
+                pan.Pantry_Id = 1;
+                pan.Name = "Bread Flour"
             }
         }
     }
